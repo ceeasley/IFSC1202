aae628fa0355f33e4ff55935b1fe14c300c1936f
ceeasley <ceeasley@ualr.edu>
2022-01-26 22:38:21 +0000

    unit 2, part 2

--- Unit 2/02.07 Reverse Three Digits.py
+++ Unit 2/02.07 Reverse Three Digits.py
@@ -1,3 +1,11 @@
 #Enter a three digit number and reverse the digits.
 #Be sure to use to .format method to create your output.
 #Use only math for your solution; do no use any string functions.
+
+x = int(input("Enter a three digit number: "))
+
+hundred = x//100
+tens = (x%100)//10
+ones = x%10
+
+print("{}{}{}".format(ones,tens,hundred))
--- Unit 2/02.08 Merge Two Numbers.py
+++ Unit 2/02.08 Merge Two Numbers.py
@@ -5,3 +5,14 @@
 #    Units digit from first number
 #    Units digit form second number
 #Use only math for your solution; do no use any string functions.
+
+a = int(input("Enter a two digit number: "))
+b = int(input("Enter another two digit number: "))
+
+tens1 = a//10
+ones1 = a%10
+
+tens2 = b//10
+ones2 = b%10
+
+print("{}{}{}{}".format(tens1,tens2,ones1,ones2))
