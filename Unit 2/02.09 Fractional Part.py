e12ab320f588f4a769f9125bd1bb1ad4c667e8eb
ceeasley <ceeasley@ualr.edu>
2022-01-27 00:00:30 +0000

    unit 2 finish

--- Unit 2/02.09 Fractional Part.py
+++ Unit 2/02.09 Fractional Part.py
@@ -1,3 +1,9 @@
 #Enter a positive real number and print its fractional part.
 #Use only math for your solution; do no use any string functions.
 #Hint: Round the result to 10 digits using the round function.
+
+x = float(input("Enter a positive real number: "))
+
+fraction = round(x%1,10)
+
+print("Fractional part: {}".format(fraction))
--- Unit 2/02.10 First Digit After Decimal.py
+++ Unit 2/02.10 First Digit After Decimal.py
@@ -1,3 +1,9 @@
 #Prompt for a positive real number
 #Print the first digit to the right of the decimal point.
 #Use only math for your solution; do no use any string functions.
+
+x = float(input("Enter a positive real number: "))
+
+deci = int((x-int(x))*10)
+
+print("{}".format(deci))
