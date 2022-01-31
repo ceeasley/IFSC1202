a1954ce21c9570f7fa664e3e2eb726e4b0d40503
ceeasley <ceeasley@ualr.edu>
2022-01-31 17:08:10 +0000

    Unit 3, part 2

--- Unit 3/03.05 Four Digit Palindrome.py
+++ Unit 3/03.05 Four Digit Palindrome.py
@@ -2,3 +2,16 @@
 #Given a four-digit integer, print "YES" if it's a palindrome and print "NO" otherwise.
 #Hint: put the units, tens, hundreds, and thousands in separate variables.
 
+x = int(input("Enter a four digit number: "))
+
+a = str(x//1000)
+b = str((x%1000)//100)
+c = str((x%100)//10)
+d = str(x%10)
+
+reverse = int(d + c + b + a)
+
+if x == reverse:
+    print("YES")
+else:
+    print("NO")
--- Unit 3/03.06 Index of Outlier.py
+++ Unit 3/03.06 Index of Outlier.py
@@ -1,3 +1,15 @@
 #Prompt for three integers: two are equal to each other and the third one is different.
 #Print the index number of the different one, either 1, 2 or 3.
 
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+c = int(input("Enter third number: "))
+
+if a == b:
+    print("3")
+elif b == c:
+    print("1")
+elif a == c:
+    print("2")
+else:
+    print("Two numbers should match.")
