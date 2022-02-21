aa5f2b6b5fb7b877bc75bc2a6bce0b5e8e8b7f60
ceeasley <ceeasley@ualr.edu>
2022-02-21 19:25:07 +0000

    Unit 6, part 2

--- Unit 6/06.02 Stock.py
+++ Unit 6/06.02 Stock.py
@@ -9,3 +9,27 @@
 #Reads the next line of input
 #Calculates and prints the stock value and the percent change from yesterday. Each column is 10 characters wide with a space between them.
 
+def percentchange(yesterday, today):
+    if yesterday != 0:
+        change = float(((today - yesterday)/yesterday)*100)
+        return change
+    else:
+        return None
+
+price_today = 0
+price_yest = 0
+
+with open("06.02 Stock.txt") as prices:
+    price = prices.readline()
+    print(f'{"Price":^10} {"Change":^10}')
+
+    while price:
+        price_yest = price_today
+        price_today = float(price)
+        percent = percentchange(price_yest, price_today)
+        if percent != None:
+            print(f'{price_today:10.2f} {percent:10.2f}')
+        else:
+            print(f'{price_today:10.2f}')
+        price = prices.readline()
+
