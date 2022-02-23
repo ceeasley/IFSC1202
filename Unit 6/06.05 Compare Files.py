da49c87d24d39f09ddbc82ae978e0bd560c3217a
ceeasley <ceeasley@ualr.edu>
2022-02-23 15:49:41 +0000

    Unit 6 Terminal issues

--- Unit 6/06.05 Compare Files.py
+++ Unit 6/06.05 Compare Files.py
@@ -13,12 +13,16 @@ with open("06.05 CompareFileA.txt") as FileA, open("06.05 CompareFileB.txt") as 
     line = 1
     difference = 0
 
-    while A and B:
-        if A != B:
-            line +=1
-            difference += 1
-            print(f'Line: {line} - File A: {A}\nLine: {line} - File B: {B}')
-        else:
-            line +=1
+    while A:
+        while B:
+            if A != B:
+                line +=1
+                difference += 1
+                print(f'Line: {line} - File A: {A}\nLine: {line} - File B: {B}')
+            else:
+                line +=1
+            A = FileA.readline()
+            B = FileB.readline()
+            
 print(f'{difference} differences')
         
